00093c0bfea2ba2e73c9eea4b071c351ff3b9399
Torsten Boettjer <31235842+tboettje@users.noreply.github.com>
2018-06-10 17:43:03 +0200

    opc

--- NewServiceUser/NewServiceUser.sh
+++ NewServiceUser/NewServiceUser.sh
@@ -1,8 +1,8 @@
 #add service group/user
 addgroup my-service
 useradd my-user --create-home --shell /bin/bash --groups my-service
-#gpasswd -a devops-service sudo #allowing sudo requires password, and not a good idea for a service account.
+#gpasswd -a my-user sudo #allowing sudo requires password, and not a good idea for a service account.
 mkdir /home/my-user/.ssh
 chmod 700 /home/my-user/.ssh
-cat my-user@v2-20150312.pub >> /home/my-user/.ssh/authorized_keys
+cat /home/opc/.ssh/authorized_keys >> /home/my-user/.ssh/authorized_keys
 chown my-user:my-user /home/my-user -R
