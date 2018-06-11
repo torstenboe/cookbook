24835e47f803266303845078eee270c1a880f9bd
Torsten Boettjer <31235842+tboettje@users.noreply.github.com>
2018-06-11 06:25:58 +0200

    group

--- NewUser/NewServiceUser.sh
+++ NewUser/NewServiceUser.sh
@@ -1,6 +1,6 @@
 #add service group/user
 groupadd <group>
-useradd -m -p <password> -s /bin/bash -g my-service <user>
+useradd -m -p <password> -s /bin/bash -g <group> <user>
 #gpasswd -a <user> sudo #allowing sudo requires password, and not a good idea for a service account.
 mkdir /home/<user>/.ssh
 chmod 700 /home/<user>/.ssh
